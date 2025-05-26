594be29b78c46a0bbb57556bffb7c7aa9880e70b
Emmalee Fecher <Eaf20202@gmail.com>
2025-05-25 22:04:35 -0400

    Added Jewlery Shop and Grab Spots

--- Assets/Scripts/Tutorial/States/LeaveTutorial.cs
+++ Assets/Scripts/Tutorial/States/LeaveTutorial.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class LeaveTutorial : AbstractManager
 {
-    string s = "Great! Now, step on the platform next to the bridge to continue to the 1st level!";
+    string s = "Great! Now, step on the platform next to the bridge and steal everything! Watch for employees though.";
     public override void EnterState(GameStateManager obj)
     {
         obj.tutorialText.text = s;
--- Assets/Scripts/Tutorial/States/UpgradeTutorial.cs
+++ Assets/Scripts/Tutorial/States/UpgradeTutorial.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class UpgradeTutorial : AbstractManager
 {
-    string s = "Move to the upgrade stand (left) and use select to upgrade your bag";
+    string s = "Sell an item, gain a point. Move to the upgrade stand (left) and use points to upgrade your bag";
     public override void EnterState(GameStateManager obj)
     {
         obj.tutorialText.text = s;
